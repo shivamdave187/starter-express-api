f02efd3f4eea8256b9a5f485d75eb809e0fe5cc5
shivamdave187 <132198482+shivamdave187@users.noreply.github.com>
2023-04-30 17:05:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+
+var requests_obj = [];
+
+app.get('/', (req, res) => {
+    if(typeof req.query.clkurl == "string"){
+        var base64string = req.query.clkurl;
+        var bufferObj = Buffer.from(base64string, "base64");
+        var decodedString = bufferObj.toString("utf8");
+        requests_obj.push(decodedString);
+        
+        res.send("Done");
+    }else{    
+        res.send("Not Done");
+    }
 })
+
+app.get("/results", (req,res) => {
+    res.send(JSON.stringify(requests_obj));
+})
+
+app.get("/results_clear", (req, res) => {
+    requests_obj = {};
+    res.send("Cleared! The current object is "+JSON.stringify(requests_obj));
+})
+
 app.listen(process.env.PORT || 3000)
