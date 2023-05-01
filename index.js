54189f7055ce8c28cc24032c3b061f24ff02e130
shivamdave187 <132198482+shivamdave187@users.noreply.github.com>
2023-05-01 15:42:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const app = express()
 var requests_obj = [];
 
 app.get('/', (req, res) => {
-    if(typeof req.query.clkurl == "string"){
+    if(typeof req.query.clkurl == "string" && typeof req.query.ip == "string"){
+        var current_request_arr = [];
+        
         var base64string = req.query.clkurl;
         var bufferObj = Buffer.from(base64string, "base64");
         var decodedString = bufferObj.toString("utf8");
-        requests_obj.push(decodedString);
+        
+        current_request_arr.push(req.query.ip);
+        current_request_arr.push(req.query.clkurl);
+        
+        requests_obj.push(current_request_arr);
         
         res.send("Done");
     }else{    
